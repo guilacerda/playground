d80bd63e49ddf68a5edf4b9e203105f03d5a6a01
guilacerda <guiglacerda@gmail.com>
2018-10-26 01:04:34 -0200

    Reads contact datas from bash (Agenda prolog)

--- prolog/agenda.pl
+++ prolog/agenda.pl
@@ -1,19 +1,37 @@
-:- dynamic contact/4.
+:- dynamic contact/2.
+:- dynamic profile/2.
 
-contact("Guila", "gui@email.com", "+5561999999999", "a").
-contact("Augusto", "augusto@email.com", "+5561999999999", "a").
-contact("Corto", "corto@email.com", "+5561999999999", "a").
-contact("Casca", "casca@email.com", "+5561999999999", "a").
-contact("Ana", "ana@email.com", "+5561999999999", "f").
+contact("Guilherme", 21).
+contact("Joao", 20).
 
-findAll(Type, Search) :- findall(Name, contact(Name, _, _, Type), Search).
+findAll(Age, Result) :- findall(Name, contact(Name, Age), Result).
 
-insertContact(Name, Email, Phone, Type) :- asserta(contact(Name, Email, Phone, Type)).
-
-findAllContacts() :-
-  contact(Name, _, _, Type),
+findAllContacts :-
+  contact(Name, _),
   write(Name),
-  write(" : "),
-  write(Type),
   nl,
   fail.
+
+insertContact(Name, Age) :- asserta(contact(Name, Age)).
+
+add_contact :-
+  nl, write('Adds your contact on agenda'), nl, nl,
+  insertDataName.
+
+insertDataName :-
+  write("What is contact name?"),
+  read(Answer),
+  asserta(profile(nameContact, Answer)),
+  profile(nameContact, NameContact),
+  insertDataAge.
+
+insertDataAge :-
+  write("What is contact age?"),
+  read(Answer),
+  asserta(profile(ageContact, Answer)),
+  profile(nameContact, NameContact),
+  profile(ageContact, AgeContact),
+  insertContact(NameContact, AgeContact),
+  findAllContacts.
+
+?- add_contact.
